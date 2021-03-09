34dc5db4bbadcee0732c2e65bb39b74315e41fd2
Kevin Chu <kvnchu2@gmail.com>
2021-03-08 18:48:57 -0800

    add psql icon

--- src/components/home/Skills.jsx
+++ src/components/home/Skills.jsx
@@ -32,7 +32,7 @@ function Skills({ heading, hardSkills, softSkills }) {
           <img src="https://github.com/kvnchu2/home/blob/master/public/rails.png?raw=true" alt="html"></img>
           <img src="https://github.com/kvnchu2/home/blob/master/public/sass.png?raw=true" alt="html"></img>
           <img src="https://github.com/kvnchu2/home/blob/master/public/mongoDB.png?raw=true" alt="html"></img>
-
+          <img src="https://github.com/kvnchu2/home/blob/master/public/psql.png?raw=true" alt="html"></img>
         </div>
         {/* <Tabs
           className="skills-tabs"
